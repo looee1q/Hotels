3bdca0243864305e4b9d2bc6f1c74ef2dfba362f
looee <valera.sevostyanov@yandex.ru>
2025-01-24 12:48:41 +0300

    Added navigation using Kotlin DSL. hotelId is passing when navigating from HotelsFragment to HotelDetailsFragment. hotelId is receiving in HotelDetailsViewModel with SavedStateHandle.

--- app/src/main/java/com/example/hotels/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/hotels/presentation/activity/MainActivity.kt
@@ -2,7 +2,15 @@ package com.example.hotels.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.fragment
+import com.example.hotels.R
 import com.example.hotels.databinding.ActivityMainBinding
+import com.example.hotels.presentation.hotelDetails.HotelDetailsFragment
+import com.example.hotels.presentation.hotels.HotelsFragment
+import com.example.hotels.presentation.navigation.HotelDetails
+import com.example.hotels.presentation.navigation.Hotels
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,5 +23,21 @@ class MainActivity : AppCompatActivity() {
             setContentView(it.root)
         }
 
+        setNavigationGraph()
+    }
+
+    private fun setNavigationGraph() {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        navController.graph = navController.createGraph(
+            startDestination = Hotels
+        ) {
+            fragment<HotelsFragment, Hotels>()
+            fragment<HotelDetailsFragment, HotelDetails>()
+        }
     }
 }
